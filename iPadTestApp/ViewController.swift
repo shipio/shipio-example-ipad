e6b6344640728b78a45131349be5231b7c7c0590
Andrew Page <andrew@andrewpage.me>
2015-01-23 11:33:06 -0800

    Initial Commit

---
+++ iPadTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPadTestApp
+//
+//  Created by Andrew Page on 1/23/15.
+//  Copyright (c) 2015 ShipIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
